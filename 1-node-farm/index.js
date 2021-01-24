2c2ac50373e226cf4a485bddea6aecb6023a969a
Vladis <vladismarkin@gmail.com>
2021-01-24 12:20:30 +0200

    Creating a Simple Web Server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,18 @@
-const fs = require("fs");
+const http = require("http");
+// const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written!");
+// const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
+
+// SERVER
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
