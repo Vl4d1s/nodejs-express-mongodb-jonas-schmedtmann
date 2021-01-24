eb711db8ceab0add9f78637577b17dc29ecc8a39
Vladis <vladismarkin@gmail.com>
2021-01-24 12:40:56 +0200

    Simple Routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const url = require("url");
+
 // const fs = require("fs");
 
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -10,7 +12,19 @@ const http = require("http");
 
 // SERVER
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server");
+  const pathName = req.url;
+
+  if (pathName === "overview" || pathName === "/") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-won-header": "hello-world",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
