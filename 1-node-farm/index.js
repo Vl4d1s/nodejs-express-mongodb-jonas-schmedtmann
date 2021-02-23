31475099e09c40ded9725b150cba4a7ed8e70cef
Vladis <vladismarkin@gmail.com>
2021-02-23 23:34:26 +0200

    finishing the boiler plate

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,7 +1,6 @@
-const http = require("http");
-const url = require("url");
-
-// const fs = require("fs");
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
@@ -11,22 +10,29 @@ const url = require("url");
 // console.log("File written!");
 
 // SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
-
-  if (pathName === "overview" || pathName === "/") {
-    res.end("This is the OVERVIEW");
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
+  if (pathName === 'overview' || pathName === '/') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else if (pathName === '/api') {
+    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+      res.writeHead(200, { 'Content-type': 'application/json' });
+      res.end(data);
+    });
   } else {
     res.writeHead(404, {
-      "Content-type": "text/html",
-      "my-won-header": "hello-world",
+      'Content-type': 'text/html',
+      'my-won-header': 'hello-world',
     });
-    res.end("<h1>Page not found</h1>");
+    res.end('<h1>Page not found</h1>');
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to request on port 8000");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to request on port 8000');
 });
